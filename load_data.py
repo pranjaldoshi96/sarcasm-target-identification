31e0f3c3fc813719e95830d316b128c6aadcb679
Pranjal Doshi <pranjald@nvidia.com>
2021-03-06 19:26:47 +0530

    modularize the code

--- load_data.py
+++ load_data.py
@@ -1,17 +1,20 @@
 import pandas as pd
 
-df1 = pd.read_excel("snippets.xlsx", engine='openpyxl')
-columns = df1.columns
+def load_data():
+    df1 = pd.read_excel("snippets.xlsx", engine='openpyxl')
+    columns = df1.columns
 
-sentence = df1[columns[0]].to_list()
-label = df1[columns[1]].to_list()
+    sentence = df1[columns[0]].to_list()
+    label = df1[columns[1]].to_list()
 
-sentence.append(columns[0])
-label.append(columns[1])
+    sentence.append(columns[0])
+    label.append(columns[1])
 
-sentence = [s.lower().replace("<em>", "") for s in sentence]
-label = [l.lower().split(',') for l in label]
-count = 1
-for s in zip(sentence, label):
-    print(count, s)
-    count += 1
+    sentence = [s.lower().replace("<em>", "") for s in sentence]
+    label = [l.lower().split(',') for l in label]
+
+    return sentence, label
+
+
+if __name__ == '__main__':
+    sentence, label = load_data()
---
+++ model.py
@@ -0,0 +1,4 @@
+import load_data
+
+sentence, label = load_data.load_data()
+print(label)

3d467f97bf4dba570cf79d4b76e73013318625b5
Pranjal Doshi <pranjald@nvidia.com>
2021-03-07 00:42:46 +0530

    Refractored load_data

--- load_data.py
+++ load_data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 def load_data():
+    """
+    Load data from snippets.xlsx and return sentence, label
+    """
     df1 = pd.read_excel("snippets.xlsx", engine='openpyxl')
     columns = df1.columns
 
@@ -17,10 +20,16 @@ def load_data():
 
 
 def annotate_data():
+    """
+        create slot tags for every sample
+        e.g. I am as hot as sun [I]
+             1 0 0 0 0 0
+    """
     train = open("train.tsv", 'w')
     train_slot = open("slots.tsv", 'w')
     #print(slot)
 
+    #load data
     sentence, label = load_data()
 
     for q in zip(sentence, label):
@@ -34,6 +43,8 @@ def annotate_data():
             slots = label.split()
 
             seqlen = len(slots)
+            # subarray matching for label
+            # Label 1 to first word 2 to follow up's
             for i in range(len(words) - seqlen + 1):
                 match = True
                 for j in range(seqlen):
@@ -46,8 +57,6 @@ def annotate_data():
                         tags[k] = 2
 
         slot = ' '.join([str(x) for x in tags]) + '\n'
-        print(s)
-        print(slot)
         train_slot.write(slot)
         train.write(s+"\n")
 

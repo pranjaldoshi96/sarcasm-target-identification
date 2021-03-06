b063c0c0bf4639853367caf2d0885aacac45bf6c
Pranjal Doshi <pranjald@nvidia.com>
2021-03-06 19:11:25 +0530

    Adding label as list

--- load_data.py
+++ load_data.py
@@ -9,8 +9,9 @@ label = df1[columns[1]].to_list()
 sentence.append(columns[0])
 label.append(columns[1])
 
+sentence = [s.lower().replace("<em>", "") for s in sentence]
+label = [l.lower().split(',') for l in label]
 count = 1
 for s in zip(sentence, label):
     print(count, s)
     count += 1
-    print()
